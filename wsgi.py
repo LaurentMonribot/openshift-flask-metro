c08ded90b90821be12a61216d73669b89d3bb98e
Laurent <laurent.monribot@gmail.com>
2018-04-25 11:29:50 +0200

    Version with a write on postgresql database Connection procedure revised for the openshift postgresql pod

--- wsgi.py
+++ wsgi.py
@@ -44,57 +44,62 @@ def stations_web_crawling():
 
 @application.route("/")
 def hello():
+    errorphrase = "Uh oh, can't connect. Invalid dbname, user or password?"
     try:
+        # BEGIN WITH THE OPENSHIFT POD
         dbname = os.environ.get("POSTGRESQL_DBNAME","NOT FOUND")
         user =  os.environ.get("POSTGRESQL_USER","NOT FOUND")
         host =  os.environ.get("POSTGRESQL_SERVICE_HOST","NOT FOUND")
         password= os.environ.get("POSTGRESQL_PASSWORD","NOT FOUND")
         port=os.environ.get("POSTGRESQL_SERVICE_PORT","NOT FOUND")
-        connect_str = "dbname={} user={} host={} port={} password={}".format(dbname,user,host,port,password)
-        # use our connection values to establish a connection
-        conn = psycopg2.connect(connect_str)
+        conn_string = "dbname={} user={} host={} port={} password={}".format(dbname,user,host,port,password)
+        # END WITH THE OPENSHIFT POD
+
+
+        # BEGIN WITHOUT THE OPENSHIFT POD
+        # # Define our connection string
+        # conn_string = "host='localhost' dbname='metro' user='metro' password='metro'"
+        #
+        # # print the connection string we will use to connect
+        # print("Connecting to database\n	->%s" % (conn_string))
+        # END WITHOUT THE OPENSHIFT POD
+
+
+        # get a connection, if a connect cannot be made an exception will be raised here
+        conn = psycopg2.connect(conn_string)
+
+        # conn.cursor will return a cursor object, you can use this cursor to perform queries
+        cursor = conn.cursor()
+        print("Connected!\n")
+
+
+        slist =  stations_web_crawling()
+        nb_stations = len(slist)
+
+
+        try:
+            for x in slist :
+                pat = "[0-9]+$"             #expression régulière à utiliser
+                str = x[1].pop()
+                nombres = re.findall("\d+", str) # recherche tous les décimals
+                numline=nombres.pop()
+
+                cursor.execute("""INSERT INTO metro_data (name,ligne) VALUES (%s,%s::integer)""", (x[0], numline))
+
+            # strquery="SELECT * FROM metro_data"
+            # cursor.execute(strquery)
+            # result=cursor.fetchall()
+            # print(result)
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            val = error
+        conn.commit()
+
+        return 'Total of stations is {}'.format(nb_stations)
+        # return 'Stations are {}'.format(result)
     except Exception as e:
-        print("Uh oh, can't connect. Invalid dbname, user or password?")
-
-    # # Define our connection string
-    # conn_string = "host='localhost' dbname='metro' user='metro' password='metro'"
-    #
-    # # print the connection string we will use to connect
-    # print("Connecting to database\n	->%s" % (conn_string))
-    #
-    # # get a connection, if a connect cannot be made an exception will be raised here
-    # conn = psycopg2.connect(conn_string)
-
-    # conn.cursor will return a cursor object, you can use this cursor to perform queries
-    cursor = conn.cursor()
-    print("Connected!\n")
-
-
-    slist =  stations_web_crawling()
-    nb_stations = len(slist)
-
-
-    try:
-        for x in slist :
-            pat = "[0-9]+$"             #expression régulière à utiliser
-            str = x[1].pop()
-            nombres = re.findall("\d+", str) # recherche tous les décimals
-            numline=nombres.pop()
-
-            cursor.execute("""INSERT INTO metro_data (name,ligne) VALUES (%s,%s::integer)""", (x[0], numline))
-
-        # strquery="SELECT * FROM metro_data"
-        # cursor.execute(strquery)
-        # result=cursor.fetchall()
-        # print(result)
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        val = error
-    conn.commit()
-
-    return 'Total of stations is {}'.format(nb_stations)
-    # return 'Stations are {}'.format(result)
-
+        print(errorphrase)
+    return errorphrase
 
 if __name__ == "__main__":
     application.run()
